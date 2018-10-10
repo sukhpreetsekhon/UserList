8adaf12afb69c4b331dda509af84df1872c11754
Sukhpreet Sekhon <sukhpreetsekhon@Sukhpreets-MacBook-Pro.local>
2018-10-09 23:57:56 -0400

    added cross oroin code

--- src/main/java/com/angularPractice/backend/Repositories/UserRepository.java
+++ src/main/java/com/angularPractice/backend/Repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.angularPractice.backend.Repositories;
 
 import com.angularPractice.backend.Models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User, String> {
 
